9667f32354668347c0c0649ff6f8f691cec5415d
Bogdan Protasenko <b.protasenko@gmail.com>
2023-09-14 18:56:55 +0200

    save2

--- src/js/partials/favorites-card.js
+++
@@ -1,4 +0,0 @@
-// import function to register Swiper custom elements
-import { register } from 'swiper/element/bundle';
-// register Swiper custom elements
-register();
--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
 import './js/utils/theme/theme';
 import './js/api/api-service';
-import './js/partials/favorites-card';
+import './js/partials/favorites-recipes';
 import './js/partials/recipes';
+
+window.addEventListener('load', function () {
+  if (document.URL.endsWith('favorites.html')) {
+    loadDataOnFavoritesPage();
+  }
+});
+
+function loadDataOnFavoritesPage() {
+  console.log('Data was loaded');
+}
+
+const currentPage = window.location.href;
+console.table(currentPage.endsWith('index.html'));
