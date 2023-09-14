bbd273706f7a644f8d39cf97e0469039b8d6ea17
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2023-09-15 02:14:13 +0300

    Create all-categories

--- src/js/refs/refs.js
+++ src/js/refs/refs.js
@@ -1,3 +1,5 @@
 export const refs = {
   body: document.querySelector('body'),
+  allCategoriesBtn: document.querySelector('.all-categories-btn'),
+  allCategoriesList: document.querySelector('.all-categories-list'),
 };
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
 import './js/utils/theme/theme';
 import './js/api/api-service';
+import './js/partials/all-categories';
 import './js/partials/recipes';
