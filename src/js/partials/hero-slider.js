b5c2735d1d9aba7b5a247e2044437f9d48436c61
MartaShchur <m.matseyko199@gmail.com>
2023-09-15 23:42:22 +0200

    add images to cards

---
+++ src/js/partials/hero-slider.js
@@ -0,0 +1,133 @@
+import { getAllEvents} from '../api/api-service'
+
+// // import Swiper JS
+// import Swiper from 'swiper';
+// // import Swiper styles
+// import 'swiper/css';
+// import 'swiper/css';
+// import 'swiper/css/pagination';
+
+// функція яка повертає картинки майстерклас
+
+const swiperContainer = document.querySelector('.swiper-wrapper')
+
+
+getAllEvents()
+  .then((data) => {
+    if (data.length === 0) {
+      return
+    }
+    swiperContainer.insertAdjacentHTML('beforeend', displayCardsInfo(data));
+  })
+  .catch ( (err)=> {
+   console.log(err)      
+  });
+
+
+
+{/* <img class="chef-photo" src="${item.cook.imgWebpUrl}"  /> */}
+
+
+function displayCardsInfo(slides) {
+  console.log(slides)
+  return slides.map(item =>
+    `<div class="swiper-wrapper">
+         
+    <div class="swiper-slide">
+    <div class="chef">
+              <img
+                class="chef-photo"
+                src="${item.cook.imgUrl}"
+                alt=""
+                width="351"
+                height="422"
+                loading="lazy"
+              />
+               <img
+               class="chef-photo"
+              src="${item.cook.imgWebpUrl}"
+              alt="img/webp"
+              width="412"
+               height="588"
+              />
+            </div>
+            <div class="dish">
+              <img
+                class="dish-photo"
+                src="${item.topic.imgUrl}"
+                alt=""
+                width="351"
+                height="422"
+              />
+              <h2 class="dish-title">${item.topic.name}</h2>
+              <p class="dish-origin">${item.topic.area}</p>
+            </div>
+            <div class="card">
+              <img
+                class="card-photo"
+                src="${item.topic.previewUrl}"
+                alt=""
+                width="351"
+                height="422"
+              />
+               <img
+                class="card-photo"
+                src="${item.topic.previewWebpUrl}"
+                alt="img/webp"
+               width="412"
+                height="588"
+               />
+            </div>
+          </div>
+        </div>`).join("");
+}
+
+
+
+
+
+/* <div class="swiper-slide">
+            <div class="chef">
+              <picture>
+                <source srcset="${cook.imgWebpUrl}" type="img/webp" />
+                <source srcset="${cook.imgUrl}" type="img/png" />
+                <img class="chef-photo" src="${cook}"  />
+              </picture>
+            </div>
+            <div class="dish">
+              <picture>
+                <source srcset="${topic.imgWebpUrl}" type="img/webp" />
+                <source srcset="${topic.imgUrl}" type="img/png" />
+                <img class="dish-photo" src="${topic}"  />
+              </picture>
+              <h2 class="dish-title">${topic.name}</h2>
+              <p class="dish-origin">${topic.area}</p>
+            </div>
+            <div class="card">
+              <picture>
+                <source srcset="${topic.previewWebpUrl}" type="img/webp" />
+                <source srcset="${topic.previewUrl}" type="img/png" />
+                <img class="card-img" src="${topic}"  />
+              </picture>
+            </div>
+          </div>
+        </div> */
+
+        
+//   ініцілізація слайдеру
+
+// const hero_slider = new Swiper('.js-hero-slider', {
+//     direction: 'vertical',
+//     loop: true,
+
+//   // If we need pagination
+//   pagination: {
+//     el: '.swiper-pagination',
+//   },
+
+//   // Default parameters
+//   slidesPerView: 1,
+//   spaceBetween: 16,
+
+// });      
+
