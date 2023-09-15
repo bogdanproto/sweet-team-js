78ec4f2bae6c500144c5638d631a5fd52a1fe3e7
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2023-09-15 22:38:39 +0300

    update recipes

--- src/js/refs/refs.js
+++ src/js/refs/refs.js
@@ -1,7 +1,12 @@
 export const refs = {
   body: document.querySelector('body'),
+  // All Categories
   allCategoriesBtn: document.querySelector('.all-categories-btn'),
   allCategoriesList: document.querySelector('.all-categories-list'),
+  // Recipes
+  recipesSection: document.querySelector('.recipes'),
+  recipesList: document.querySelector('.recipe-list'),
+  recipesGuard: document.querySelector('.recipes-guard'),
 
   //favorites page
   filterFavorites: document.querySelector('.favorites-cards-filter'),
@@ -10,4 +15,4 @@ export const refs = {
   ),
   emptyContainerFavorites: document.querySelector('.favorites-empty-container'),
   heroContainerFavorites: document.querySelector('.favorites-hero'),
-};
+}
