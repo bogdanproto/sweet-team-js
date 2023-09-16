f75713538f87ed3271cee5c3d8850012ec91ebcd
bogdanproto <129848498+bogdanproto@users.noreply.github.com>
2023-09-16 14:00:48 +0000

    Deploying to gh-pages from @ bogdanproto/sweet-team-js@d23812c3a986769fc127cfc3fa92455536911a83 🚀

--- assets/main-18bed455.js
+++ assets/main-18bed455.js
@@ -10,9 +10,11 @@ Notiflix needs to be appended to the "<`+r+'>" element, but you called it before
 
 <label class="recipe-heart-label">
   <input type="checkbox" class="recipe-heart-checkbox">
-  <svg class="recipe-heart-icon" width="22" height="22">
-    <use href="../../../img/sprite.svg#icon-heart"></use>
-  </svg>
+  
+  <svg class="recipe-heart-icon" width="22" height="20" viewBox="0 0 22 20" xmlns="http://www.w3.org/2000/svg">
+    <path opacity="0.9" fill-rule="evenodd" clip-rule="evenodd" d="M10.9934 3.70783C9.16066 1.5652 6.10444 0.988839 3.80814 2.95085C1.51185 4.91285 1.18856 8.19323 2.99186 10.5137C4.49117 12.443 9.02863 16.5121 10.5158 17.8291C10.6821 17.9764 10.7653 18.0501 10.8624 18.0791C10.9471 18.1043 11.0397 18.1043 11.1244 18.0791C11.2215 18.0501 11.3046 17.9764 11.471 17.8291C12.9582 16.5121 17.4956 12.443 18.9949 10.5137C20.7982 8.19323 20.5144 4.89221 18.1786 2.95085C15.8429 1.00948 12.8261 1.5652 10.9934 3.70783Z"/>
+</svg>
+
 </label>
 
 <div class="recipe-info">
