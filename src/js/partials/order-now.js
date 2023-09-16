19e0fecb68fcbadad6e6111d08f90fafb07c5022
Ruslan Tretiak <99765530+Ruslan-Tretiak@users.noreply.github.com>
2023-09-16 12:47:53 +0100

    Check

--- src/js/partials/order-now.js
+++ src/js/partials/order-now.js
@@ -1,3 +1,4 @@
+// ==================Modal window=============
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open]'),
@@ -13,3 +14,47 @@
     document.body.classList.toggle('no-scroll');
   }
 })();
+
+
+// ===============Send to beck-end info=========================
+
+document.addEventListener('DOMContentLoaded', function() {
+  const form = document.querySelector('.callback-form');
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const name = document.getElementById('buyer-name').value;
+    const phone = document.getElementById('buyer-phone').value;
+    const email = document.getElementById('buyer-email').value;
+    const comments = document.getElementById('buyer-comment').value;
+
+    const data = {
+      name: name,
+      phone: phone,
+      email: email,
+      comments: comments
+    };
+
+    fetch('https://tasty-treats-backend.p.goit.global/api/orders', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Success:', data);
+
+      // Clear form
+      form.reset();
+
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      // Error message
+    });
+  });
+});
+
