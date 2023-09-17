9444d8119ca3e5d9a6605667a22f70bf6c59eb6f
Nadiia17 <nababiy@gmail.com>
2023-09-18 02:28:23 +0300

    upd

--- src/js/partials/order-now.js
+++ src/js/partials/order-now.js
@@ -1,49 +1,64 @@
 import { refs } from '../refs/refs';
 import { addOrder } from '../api/api-service';
+import { Notify } from 'notiflix';
 
-const toggleModal = () => {
-  refs.modal.classList.toggle('is-hidden');
-  document.body.classList.toggle('no-scroll');
+const form = document.querySelector('.callback-form');
+
+const handleSubmit = async function (event) {
+  event.preventDefault();
+
+  const name = document.getElementById('buyer-name').value;
+  const phone = document.getElementById('buyer-phone').value;
+  const email = document.getElementById('buyer-email').value;
+  const comment =
+    document.getElementById('buyer-comment').value || 'No comment provided';
+
+  const data = {
+    name: name,
+    phone: phone,
+    email: email,
+    comment: comment,
+  };
+
+  try {
+    const result = await addOrder(data);
+    Notify.success("Thank you for your order! We'll be in touch shortly.");
+    console.log(result);
+    console.log(data);
+    toggleModal();
+    form.reset();
+  } catch (error) {
+    console.error('Error while adding order:', error.message);
+    console.log('Server response:', error.response.data);
+    Notify.failure('Oops, something went wrong. Please reloade the page');
+  }
+};
+
+const onEscKeyPress = function (event) {
+  if (event.code === 'Escape') {
+    toggleModal();
+  }
+};
+
+const onBackdropClick = event => {
+  if (event.target === event.currentTarget) {
+    toggleModal();
+  }
 };
 
 export function onShowOrderForm() {
   refs.modal.classList.toggle('is-hidden');
   refs.closeModalBtn.addEventListener('click', toggleModal);
-  sendForm();
-}
-
-function sendForm() {
-  const form = document.querySelector('.callback-form');
-
-  const handleSubmit = async function (event) {
-    event.preventDefault();
-
-    const name = document.getElementById('buyer-name').value;
-    const phone = document.getElementById('buyer-phone').value;
-    const email = document.getElementById('buyer-email').value;
-    const comments = document.getElementById('buyer-comment').value;
-
-    const data = {
-      name: name,
-      phone: phone,
-      email: email,
-      comments: comments,
-    };
-
-      const result = addOrder(data);
-
-  }
-
-  form.addEventListener('submit', handleSubmit);
-
-  const onEscKeyPress = function (event) {
-    if (event.code === 'Escape') {
-      toggleModal();
-      // Remove event listener
-      document.removeEventListener('keydown', onEscKeyPress);
-      refs.closeModalBtn.removeEventListener('click', toggleModal);
-    }
-  };
-
   document.addEventListener('keydown', onEscKeyPress);
+  form.addEventListener('submit', handleSubmit);
+  refs.modal.addEventListener('click', onBackdropClick);
 }
+
+const toggleModal = () => {
+  refs.modal.classList.toggle('is-hidden');
+  document.body.classList.toggle('no-scroll');
+  document.removeEventListener('keydown', onEscKeyPress);
+  refs.closeModalBtn.removeEventListener('click', toggleModal);
+  form.removeEventListener('submit', handleSubmit);
+  refs.modal.removeEventListener('click', onBackdropClick);
+};
