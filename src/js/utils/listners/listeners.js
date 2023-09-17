7deab299a04aa036f6776dab4838680bed18e101
LuckyPhoenix7 <a.lubashov@gmail.com>
2023-09-17 18:35:36 +0200

    ModalRecipe v4.0

--- src/js/utils/listners/listeners.js
+++
@@ -1,6 +0,0 @@
-import { refs } from '../../refs/refs';
-import { onShowOrderForm } from '../../partials/order-now';
-
-export function onSwitchHomeListners() {
-  refs.heroBtn.addEventListener('click', onShowOrderForm);
-}
