87c996f05e9cd7ede0fee4aab3b72c938c0b1a4d
Nadiia17 <nababiy@gmail.com>
2023-09-17 19:08:43 +0300

    upd

--- src/js/utils/markup/hero-events.js
+++ src/js/utils/markup/hero-events.js
@@ -1,25 +1,33 @@
 export function createMarkupHeroEvents(arr) {
   return arr
     .map(
-      ({
-        cook,
-        topic,
-      }) => `<div class="swiper-slide hero-event"><div class="hero-event">
-      <div class="hero-block-cook">
-        <img class="hero-block-cook-img" src="${cook.imgUrl}" alt="">
-      </div>
-      <div class="hero-block-event">
-      <div class ="hero-block-event-thumb">
-        <img class="hero-block-event-img" src="${topic.previewUrl}" alt="" />
-        <p class="hero-block-event-title">${topic.name}</p>
-        <p class="hero-block-event-text">${topic.area}</p>
-        </div>
-      </div>
-      <div class="hero-block-meal">
-        <img class="hero-block-meal-img" src="${topic.imgUrl}" alt="">
-      </div>
-      </div>
-    </div>`
+      ({ cook, topic }) => `
+                <div class="swiper-slide hero-event">
+                    <div class="hero-event">
+                        <div class="hero-block-cook">
+                            <picture>
+                                <source srcset="${cook.imgWebpUrl}" type="image/webp" />
+                                <img class="hero-block-cook-img" src="${cook.imgUrl}" alt="${cook.name}" loading="lazy" />
+                            </picture>
+                        </div>
+                        <div class="hero-block-event">
+                            <div class="hero-block-event-thumb">
+                                <picture>
+                                    <source srcset="${topic.previewWebpUrl}" type="image/webp" />
+                                    <img class="hero-block-event-img" src="${topic.previewUrl}" alt="${topic.name}" loading="lazy" />
+                                </picture>
+                                <p class="hero-block-event-title">${topic.name}</p>
+                                <p class="hero-block-event-text">${topic.area}</p>
+                            </div>
+                        </div>
+                        <div class="hero-block-meal">
+                            <picture>
+                                <source srcset="${topic.imgWebpUrl}" type="image/webp" />
+                                <img class="hero-block-meal-img" src="${topic.imgUrl}" alt="${topic.name}" loading="lazy" />
+                            </picture>
+                        </div>
+                    </div>
+                </div>`
     )
     .join('');
 }
