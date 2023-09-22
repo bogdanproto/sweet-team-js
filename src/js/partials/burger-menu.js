bb4e452959bc2ba81f64a124ae8ac8b6724167bd
SergiiBrusov <127529976+SergiiBrusov@users.noreply.github.com>
2023-09-22 07:46:54 +0200

    header current page

--- src/js/partials/burger-menu.js
+++ src/js/partials/burger-menu.js
@@ -20,3 +20,6 @@ const closeMenu = () => {
 
   refs.closeMenuBtn.removeEventListener('click', closeMenu);
 };
+
+
+
--- src/js/utils/current-page.js
+++ src/js/utils/current-page.js
@@ -2,3 +2,16 @@ export function getCurrentPage() {
   return window.location.href;
   // console.table(currentPage.endsWith('index.html'));
 }
+
+const currentPageUrl = window.location.href;
+
+
+const homeLink = document.getElementById('Home-header');
+const favoritesLink = document.getElementById('Favs-header');
+
+
+if (currentPageUrl.includes('index.html')) {
+  homeLink.classList.add('active');
+} else if (currentPageUrl.includes('favorites.html')) {
+  favoritesLink.classList.add('active');
+}
